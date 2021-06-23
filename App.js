32d3c93898805ddadbdbf9758333da8a42001599
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-06-23 02:31:48 -0300

    se limpio un poco el código

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import * as React from "react";
 import { AppRegistry } from "react-native";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 import { name as appName } from "./app.json";
 
-//import WelcomeScreen from "./app/screens/WelcomeScreen";
-//import MyComponent from "./MyComponent";
 import NavigationApp from "./NavigationApp";
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "tomato",
-    accent: "yellow",
-  },
-};
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
-      {/* <WelcomeScreen /> */}
-      {/* <MyComponent /> */}
+    <PaperProvider>
       <NavigationApp />
     </PaperProvider>
   );
